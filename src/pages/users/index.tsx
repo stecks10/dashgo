7ed80f0d2f34f55e582ffc0f1031b730d7e2f6be
Vitor Nunes <stecks10@gmail.com>
2021-10-12 17:36:36 -0300

    Factories e seeds

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -18,6 +18,7 @@ import { RiAddLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
+import { useEffect } from 'react';
 import Link from 'next/link';
 
 export default function UserList() {
@@ -25,6 +26,13 @@ export default function UserList() {
     base: false,
     lg: true,
   });
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  });
+
   return (
     <Box>
       <Header />
